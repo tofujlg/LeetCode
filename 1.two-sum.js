7ffda2052d43f526056eac1aeaf888b280a82bff
Kohei Okumura <tofuredbull@gmail.com>
2024-07-12 12:48:36 +0900

    Solve

---
+++ 1.two-sum.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let i, j;
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+// @lc code=end
